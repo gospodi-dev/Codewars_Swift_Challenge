591f7889d29fd351c04a829e085e3c6a1c660886
Gospodi <gospodijazzet@gmail.com>
2022-07-05 15:29:57 +0300

    добавлена КАТА (8 kyu) - Opposite number: Противоположный номер (условие)

--- 8 kyu/01. Opposite_Number_8kyu.playground/Contents.swift
+++ 8 kyu/01. Opposite_Number_8kyu.playground/Contents.swift
@@ -1,3 +1,65 @@
 import Foundation
 
+/*:
+ ### Opposite number: Противоположный номер
+ 
+ Very simple, given an integer or a floating-point number, find its opposite (Очень просто, учитывая целое число или число с плавающей запятой, найдите его противоположность).
+ Примеры:
+ ```swift
+ 1: -1
+ 14: -14
+ -34: 34
+ ```
+ Начальная конструкция:
+ ```swift
+ func opposite(number: Double) -> Double {
+   # your solution here
+ }
+ ```
+ */
 
+// Решение
+func opposite(number: Double) -> Double {
+  # your solution here
+}
+
+
+
+// ссылка на КАТА - https://www.codewars.com/kata/56dec885c54a926dcd001095/train/swift
+
+/*:
+ ### Тесты
+ ```swift
+ import XCTest
+
+ class SolutionTest: XCTestCase {
+     static var allTests = [
+         ("Basic test", testExample),
+     ]
+
+     func testExample() {
+       XCTAssertEqual(opposite(number: 1), -1)
+     }
+ }
+
+ XCTMain([
+     testCase(SolutionTest.allTests)
+ ])
+ ```
+*/
+
+
+/*:
+ ### Лучшие практики
+ Варианты решения от сообщества
+ 
+ ```swift
+
+ ```
+ ```swift
+
+ ```
+ ```swift
+
+ ```
+*/
