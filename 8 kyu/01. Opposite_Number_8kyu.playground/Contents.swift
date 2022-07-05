a22ff0fbfa7d2e3e27a73e0df773cf6e0ec6c392
Gospodi <gospodijazzet@gmail.com>
2022-07-05 16:00:42 +0300

    добавлена КАТА (8 kyu) - Opposite number: Противоположный номер (решение)

--- 8 kyu/01. Opposite_Number_8kyu.playground/Contents.swift
+++ 8 kyu/01. Opposite_Number_8kyu.playground/Contents.swift
@@ -20,10 +20,8 @@ import Foundation
 
 // Решение
 func opposite(number: Double) -> Double {
-  # your solution here
+    return -number
 }
-
-
 
 // ссылка на КАТА - https://www.codewars.com/kata/56dec885c54a926dcd001095/train/swift
 
@@ -54,12 +52,33 @@ func opposite(number: Double) -> Double {
  Варианты решения от сообщества
  
  ```swift
-
+ func opposite(number: Double) -> Double {
+   // your solution here
+   return number * -1.0
+ }
  ```
  ```swift
-
+ func opposite(number: Double) -> Double {
+   var x = number
+   x.negate()
+   return x
+ }
  ```
  ```swift
-
+ func opposite(number: Double) -> Double {
+   -number
+ }
+ ```
+ ```swift
+ func opposite(number: Double) -> Double {
+   return Double(number - number * 2)
+ }
+ ```
+ ```swift
+ func opposite(number: Double) -> Double {
+   return 0 - number
+ }
  ```
 */
+#imageLiteral(resourceName: "Снимок экрана 2022-07-05 в 15.55.31.png")
+#imageLiteral(resourceName: "Снимок экрана 2022-07-05 в 15.57.20.png")
